951952e1ae5517affeb1fb947e5f4990841235d1
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-06-13 22:12:01 +0800

    hm2014811: Update blobs
    * Fixes camera and OMX.
    * Use prebuilt media libs as the media-caf-new libs conflict
    with the other media libs from stock.
    * Remove unnecessary modules.
    * Add libs for camera.
    
    Change-Id: I1cda867b59260b947eaccbbe4ff8573f76023dc2

--- device.mk
+++ device.mk
@@ -157,20 +157,20 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
 # OMX
-PRODUCT_PACKAGES += \
-    libdashplayer \
-    libOmxAacEnc \
-    libOmxAmrEnc \
-    libOmxCore \
-    libOmxEvrcEnc \
-    libOmxQcelp13Enc \
-    libOmxVdec \
-    libOmxVenc \
-    libstagefrighthw \
-    qcmediaplayer
-
-PRODUCT_BOOT_JARS += \
-    qcmediaplayer
+#PRODUCT_PACKAGES += \
+#    libdashplayer \
+#    libOmxAacEnc \
+#    libOmxAmrEnc \
+#    libOmxCore \
+#    libOmxEvrcEnc \
+#    libOmxQcelp13Enc \
+#    libOmxVdec \
+#    libOmxVenc \
+#    libstagefrighthw \
+#    qcmediaplayer
+#
+#PRODUCT_BOOT_JARS += \
+#    qcmediaplayer
 
 # Power HAL
 PRODUCT_PACKAGES += \
