2ecf178fd0bdd0a1c4aaecfaea1733e4f153bf81
Zhao Wei Liew <zhaoweiliew@gmail.com>
2015-06-15 09:52:18 +0800

    hm2014811: Disable libm optimizations
    * Also remove unnecessary flag (enabled by default)
    
    Change-Id: I78cfd18c3a48ab57a36a42535c00d6d3a10267c5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,9 +30,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
 TARGET_CPU_VARIANT := cortex-a53
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
